860c66b45ff5fe309cd92faa7c40a3ef01fd52be
Pravin Dahal <pravin.dahal@optimeering.com>
2022-06-09 09:43:59 +0200

    Put version before license

--- Formula/arcl.rb
+++ Formula/arcl.rb
@@ -2,8 +2,8 @@ class Arcl < Formula
   desc "CLI tool for the Optimeering Archimedes Platform"
   homepage "https://www.optimeering.com/"
   url "https://github.com/OptimeeringAS/homebrew-archimedes/raw/master/bin/arcl-v1.7.0.zip"
-  license "Apache-2.0"
   version "1.7.0"
+  license "Apache-2.0"
   sha256 "6589281752a89cf3571bccefc21f812dd3d875ea1c585cbf563eef722dde7ed8"
 
   def install
