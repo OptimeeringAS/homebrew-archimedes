98221812bbd3d769e3e49ccd030755be2831ed09
Pravin Dahal <pravin.dahal@optimeering.com>
2022-06-09 09:45:37 +0200

    Put sha256 before license

--- Formula/arcl.rb
+++ Formula/arcl.rb
@@ -3,8 +3,8 @@ class Arcl < Formula
   homepage "https://www.optimeering.com/"
   url "https://github.com/OptimeeringAS/homebrew-archimedes/raw/master/bin/arcl-v1.7.0.zip"
   version "1.7.0"
-  license "Apache-2.0"
   sha256 "6589281752a89cf3571bccefc21f812dd3d875ea1c585cbf563eef722dde7ed8"
+  license "Apache-2.0"
 
   def install
     bin.install "arcl"
