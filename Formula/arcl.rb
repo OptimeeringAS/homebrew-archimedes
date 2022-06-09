de63f8608560f5c7fdfc111e7fb3b27dc1e396d7
Pravin Dahal <pravin.dahal@optimeering.com>
2022-06-09 09:40:28 +0200

    Fix formatting of formula file

--- Formula/arcl.rb
+++ Formula/arcl.rb
@@ -1,10 +1,10 @@
 class Arcl < Formula
-  desc "The CLI for the Optimeering Archimedes Platform"
+  desc "CLI tool for the Optimeering Archimedes Platform"
   homepage "https://www.optimeering.com/"
   url "https://github.com/OptimeeringAS/homebrew-archimedes/raw/master/bin/arcl-v1.7.0.zip"
-  sha256 "6589281752a89cf3571bccefc21f812dd3d875ea1c585cbf563eef722dde7ed8"
-  license "Apache 2.0"
+  license "Apache-2.0"
   version "1.7.0"
+  sha256 "6589281752a89cf3571bccefc21f812dd3d875ea1c585cbf563eef722dde7ed8"
 
   def install
     bin.install "arcl"
