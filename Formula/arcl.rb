3f52573a84a484eab91fe76d19b247a9c462e8da
Pravin Dahal <pravin.dahal@optimeering.com>
2022-06-09 09:36:31 +0200

    Move arcl.zip to bin/arcl-v1.7.0.zip

--- Formula/arcl.rb
+++ Formula/arcl.rb
@@ -1,7 +1,7 @@
 class Arcl < Formula
   desc "The CLI for the Optimeering Archimedes Platform"
   homepage "https://www.optimeering.com/"
-  url "https://github.com/OptimeeringAS/homebrew-archimedes/raw/master/arcl.zip"
+  url "https://github.com/OptimeeringAS/homebrew-archimedes/raw/master/bin/arcl-v1.7.0.zip"
   sha256 "6589281752a89cf3571bccefc21f812dd3d875ea1c585cbf563eef722dde7ed8"
   license "Apache 2.0"
   version "1.7.0"
