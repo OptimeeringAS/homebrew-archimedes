7d6f848f3067acb1aa75bd42489c8a14dbff3f3e
Optimeering AS (auto) <dev@optimeering.com>
2022-12-16 12:42:49 +0000

    Update arcl formula to version v1.7.13

--- Formula/arcl.rb
+++ Formula/arcl.rb
@@ -1,8 +1,8 @@
 class Arcl < Formula
   desc "CLI tool for the Optimeering Archimedes Platform"
   homepage "https://www.optimeering.com/"
-  url "https://github.com/OptimeeringAS/homebrew-archimedes/releases/download/v1.7.11/arcl-v1.7.11.zip"
-  sha256 "0e5754662447b30452c698e0d0664f10ffdd69b41ca0381ef779b58fd8f3b037"
+  url "https://github.com/OptimeeringAS/homebrew-archimedes/releases/download/v1.7.13/arcl-v1.7.13.zip"
+  sha256 "0f0e13725e2ebb15fab0f917b83f9d23b97abcc61fb66c32abb298717d04ad7f"
   license "Apache-2.0"
 
   def install
